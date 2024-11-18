62b1ee795890ca6d15108d09016de884fa0baba7
SeniorCUCUMBERDeveloper <kalinenkov.a@bk.ru>
2024-11-19 01:05:22 +0300

    Update

--- Test/src/generator/RandomGenerator.java
+++ Test/src/generator/RandomGenerator.java
@@ -4,6 +4,6 @@ import java.util.Random;
 public class RandomGenerator {
     public static int generateNumber() {
         Random random = new Random();
-        return random.nextInt(4) + 2; // nextInt(4) генерирует 0, 1, 2, 3;  +2 сдвигает диапазон
+        return random.nextInt(4) + 2;
     }
 }
